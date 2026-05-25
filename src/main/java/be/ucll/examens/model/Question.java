ffa58031c1ce37c92abdd3779bc65eaa7e61da27
Thomas-Vanhecke <thomasvanhecke18@gmail.com>
2026-05-25 23:26:06 +0200

    Update Question.java

--- src/main/java/be/ucll/examens/model/Question.java
+++ src/main/java/be/ucll/examens/model/Question.java
@@ -23,10 +23,9 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String explanation;
 
-
-
-
     private boolean multiSelect;
+
+    private String category;
 
     @BatchSize(size = 34)
     @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
@@ -36,85 +35,31 @@ public class Question {
     @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     private List<Answer> answers;
 
-
-
     // constructors
-    public Question() {
-    }
+    public Question() {}
 
+    // getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    //gevulde constructor
+    public String getQuestion() { return question; }
+    public void setQuestion(String question) { this.question = question; }
 
-    public Question(Long id, String question, String code, String explanation, boolean multiSelect, List<Option> options, List<Answer> answers) {
-        this.id = id;
-        this.question = question;
-        this.code = code;
-        this.explanation = explanation;
-        this.multiSelect = multiSelect;
-        this.options = options;
-        this.answers = answers;
-    }
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
 
+    public String getExplanation() { return explanation; }
+    public void setExplanation(String explanation) { this.explanation = explanation; }
 
-    // getters en setters via rechtermuisknop → Generate
+    public boolean isMultiSelect() { return multiSelect; }
+    public void setMultiSelect(boolean multiSelect) { this.multiSelect = multiSelect; }
 
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
 
-    public Long getId() {
-        return id;
-    }
+    public List<Option> getOptions() { return options; }
+    public void setOptions(List<Option> options) { this.options = options; }
 
-    public String getQuestion() {
-        return question;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getExplanation() {
-        return explanation;
-    }
-
-    public boolean isMultiSelect() {
-        return multiSelect;
-    }
-
-    public List<Option> getOptions() {
-        return options;
-    }
-
-    public List<Answer> getAnswers() {
-        return answers;
-    }
-
-
-    //setters
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setExplanation(String explanation) {
-        this.explanation = explanation;
-    }
-
-    public void setMultiSelect(boolean multiSelect) {
-        this.multiSelect = multiSelect;
-    }
-
-    public void setOptions(List<Option> options) {
-        this.options = options;
-    }
-
-    public void setAnswers(List<Answer> answers) {
-        this.answers = answers;
-    }
+    public List<Answer> getAnswers() { return answers; }
+    public void setAnswers(List<Answer> answers) { this.answers = answers; }
 }
